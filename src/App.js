1d6f6c4e957d51268b7f41a49ac785146fd12446
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-09-30 21:15:46 +0300

    h

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,51 @@
+import styles from '../Profile/Profile.module.css';
+import PropTypes from 'prop-types';
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div class={styles.profile}>
+      <div class={styles.description}>
+        <img src={avatar} alt="Аватар пользователя" class={styles.avatar} />
+        <p class={styles.name}>{name}</p>
+        <p class={styles.tag}>@{tag}</p>
+        <p class={styles.location}>{location}</p>
+      </div>
+      <ul class={styles.stats}>
+        <li>
+          <span class={styles.label}>Followers</span>
+          <span class={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span class={styles.label}>Views</span>
+          <span class={styles.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span class={styles.label}>Likes</span>
+          <span class={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
+
+Profile.defaultProps = {
+  name: 'User name',
+  tag: '@User tag',
+  location: 'User location',
+  avatar: './src/photo1.jpeg',
+  stats: {
+    followers: '0',
+    views: '0',
+    likes: '0',
+  },
+};
+
+export default Profile;
